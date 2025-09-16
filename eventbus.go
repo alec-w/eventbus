51e63f03cb60e9345939f20bbe75deaed75e1857
Alec Wilson <alecw1992@gmail.com>
2025-09-16 23:55:26 +0100

    Fix formatting after testing formatting workflow job

--- eventbus.go
+++ eventbus.go
@@ -96,8 +96,7 @@ func (e *EventBus[T]) Subscribe(
 	subscription := &subscription[T]{
 		events: make(chan T),
 	}
-	e.subscriptions[subscription] = struct {
-	}{}
+	e.subscriptions[subscription] = struct{}{}
 	done := make(chan struct{})
 	filteredEvents := make(chan T)
 	go func() {
